ad7cb45106167b9d57700330bc3e5ee4d7bf720e
ksmin23 <sonagy23@gmail.com>
2020-10-13 07:54:10 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-aws-cdk-examples
---
+++ elasticsearch/app.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+from aws_cdk import (
+  core,
+  aws_ec2,
+  aws_iam, 
+  aws_elasticsearch
+)
+
+class ElasticsearchStack(core.Stack):
+
+  def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    super().__init__(scope, id, **kwargs)
+
+    # The code that defines your stack goes here
+    # The code that defines your stack goes here
+    vpc = aws_ec2.Vpc(self, "AnalyticsWorkshopVPC",
+      max_azs=2,
+      gateway_endpoints={
+        "S3": aws_ec2.GatewayVpcEndpointOptions(
+          service=aws_ec2.GatewayVpcEndpointAwsService.S3
+        )
+      }
+    )
+
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/InstanceClass.html
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/InstanceSize.html#aws_cdk.aws_ec2.InstanceSize
+    ec2_instance_type = aws_ec2.InstanceType.of(aws_ec2.InstanceClass.BURSTABLE3, aws_ec2.InstanceSize.MEDIUM)
+
+    sg_bastion_host = aws_ec2.SecurityGroup(self, "BastionHostSG",
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for an bastion host',
+      security_group_name='bastion-host-sg'
+    )
+    core.Tags.of(sg_bastion_host).add('Name', 'bastion-host-sg')
+
+    #XXX: As there are no SSH public keys deployed on this machine,
+    # you need to use EC2 Instance Connect with the command
+    #  'aws ec2-instance-connect send-ssh-public-key' to provide your SSH public key.
+    # https://aws.amazon.com/de/blogs/compute/new-using-amazon-ec2-instance-connect-for-ssh-access-to-your-ec2-instances/
+    bastion_host = aws_ec2.BastionHostLinux(self, "BastionHost",
+      vpc=vpc,
+      instance_type=ec2_instance_type,
+      subnet_selection=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC),
+      security_group=sg_bastion_host
+    )
+
+    #TODO: SHOULD restrict IP range allowed to ssh acces
+    bastion_host.allow_ssh_access_from(aws_ec2.Peer.ipv4("0.0.0.0/0"))
+
+    sg_use_es = aws_ec2.SecurityGroup(self, "ElasticSearchClientSG",
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for an elasticsearch client',
+      security_group_name='use-es-cluster-sg'
+    )
+    core.Tags.of(sg_use_es).add('Name', 'use-es-cluster-sg')
+
+    sg_es = aws_ec2.SecurityGroup(self, "ElasticSearchSG",
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for an elasticsearch cluster',
+      security_group_name='es-cluster-sg'
+    )
+    core.Tags.of(sg_es).add('Name', 'es-cluster-sg')
+
+    sg_es.add_ingress_rule(peer=sg_es, connection=aws_ec2.Port.all_tcp(), description='es-cluster-sg')
+    sg_es.add_ingress_rule(peer=sg_use_es, connection=aws_ec2.Port.all_tcp(), description='use-es-cluster-sg')
+    sg_es.add_ingress_rule(peer=sg_bastion_host, connection=aws_ec2.Port.all_tcp(), description='bastion-host-sg')
+
+    #XXX: aws cdk elastsearch example - https://github.com/aws/aws-cdk/issues/2873
+    es_domain_name = 'es-hol'
+    es_cfn_domain = aws_elasticsearch.CfnDomain(self, "ElasticSearch",
+      elasticsearch_cluster_config={
+        "dedicatedMasterCount": 3,
+        "dedicatedMasterEnabled": True,
+        "dedicatedMasterType": "t2.medium.elasticsearch",
+        "instanceCount": 2,
+        "instanceType": "t2.medium.elasticsearch",
+        "zoneAwarenessEnabled": True
+      },
+      ebs_options={
+        "ebsEnabled": True,
+        "volumeSize": 10,
+        "volumeType": "gp2"
+      },
+      domain_name=es_domain_name,
+      elasticsearch_version="7.7",
+      encryption_at_rest_options={
+        "enabled": False
+      },
+      access_policies={
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Principal": {
+              "AWS": "*"
+            },
+            "Action": [
+              "es:Describe*",
+              "es:List*",
+              "es:Get*",
+              "es:ESHttp*"
+            ],
+            "Resource": self.format_arn(service="es", resource="domain", resource_name="{}/*".format(es_domain_name))
+          }
+        ]
+      },
+      snapshot_options={
+        "automatedSnapshotStartHour": 17
+      },
+      vpc_options={
+        "securityGroupIds": [sg_es.security_group_id],
+        "subnetIds": vpc.select_subnets(subnet_type=aws_ec2.SubnetType.PRIVATE).subnet_ids
+      }
+    )
+    core.Tags.of(es_cfn_domain).add('Name', 'es-hol')
+
+
+app = core.App()
+ElasticsearchStack(app, "elasticsearch")
+
+app.synth()
+
