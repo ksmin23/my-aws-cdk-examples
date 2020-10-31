93334e7145d0a4594ee55839704f4ff30f05f729
ksmin23 <sonagy23@gmail.com>
2020-10-31 18:35:47 +0900

    use existed Secrets for DB cluser and Proxy

--- rds/aurora-mysql/app.py
+++ rds/aurora-mysql/app.py
@@ -40,10 +40,9 @@ class AuroraMysqlStack(core.Stack):
       description='security group for mysql',
       security_group_name='default-mysql-server'
     )
-    core.Tags.of(sg_mysql_server).add('Name', 'mysql-server')
-
     sg_mysql_server.add_ingress_rule(peer=sg_use_mysql, connection=aws_ec2.Port.tcp(3306),
       description='use-default-mysql')
+    core.Tags.of(sg_mysql_server).add('Name', 'mysql-server')
 
     rds_subnet_group = aws_rds.SubnetGroup(self, 'RdsSubnetGroup',
       description='subnet group for mysql',
@@ -79,23 +78,13 @@ class AuroraMysqlStack(core.Stack):
     db_cluster_name = self.node.try_get_context('db_cluster_name')
     #XXX: aws_rds.Credentials.from_username(username, ...) can not be given user specific Secret name
     #XXX: therefore, first create Secret and then use it to create database
-    db_secret_name = '{kind}/{dbCluster}/{rdsType}'.format(kind='prod',
-      dbCluster=db_cluster_name, rdsType='AuroraMySQL')
-    aws_secretsmanager.Secret(self, 'TemplatedDBSecret',
-      description='automatically generated database secret',
-      secret_name=db_secret_name,
-      generate_secret_string=aws_secretsmanager.SecretStringGenerator(
-        secret_string_template=json.dumps({"username": "admin"}),
-        generate_string_key='password',
-        exclude_characters=''' "%+~`#$&*()|[]{}:;<>?!'/''',
-        exclude_punctuation=True
-      ),
-      removal_policy=core.RemovalPolicy.RETAIN
-    )
-    db_secret = aws_secretsmanager.Secret.from_secret_name(self, 'DBSecret', db_secret_name)
+    db_secret_name = self.node.try_get_context('db_secret_name')
+    db_secret_arn = self.format_arn(region=core.Aws.REGION, resource='secret',
+      service='secretsmanager', resource_name=db_secret_name)
+    db_secret = aws_secretsmanager.Secret.from_secret_arn(self, 'DBSecretFromArn', db_secret_arn)
     rds_credentials = aws_rds.Credentials.from_secret(db_secret)
 
-    cluster = aws_rds.DatabaseCluster(self, 'Database',
+    db_cluster = aws_rds.DatabaseCluster(self, 'Database',
       engine=rds_engine,
       credentials=rds_credentials,
       instance_props={
@@ -112,7 +101,34 @@ class AuroraMysqlStack(core.Stack):
       parameter_group=rds_cluster_param_group,
       cloudwatch_logs_retention=aws_logs.RetentionDays.THREE_DAYS,
       cluster_identifier=db_cluster_name,
-      subnet_group=rds_subnet_group
+      subnet_group=rds_subnet_group,
+      backup=aws_rds.BackupProps(
+        retention=core.Duration.days(3),
+        preferred_window="03:00-04:00"
+      )
+    )
+    
+    sg_mysql_public_proxy = aws_ec2.SecurityGroup(self, 'MySQLPublicProxySG',
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for mysql public proxy',
+      security_group_name='default-mysql-public-proxy'
+    )
+    sg_mysql_public_proxy.add_ingress_rule(peer=aws_ec2.Peer.any_ipv4(), connection=aws_ec2.Port.tcp(3306), description='mysql public proxy')
+    core.Tags.of(sg_mysql_public_proxy).add('Name', 'mysql-public-proxy')
+
+    #XXX: Datbase Proxy use only Secret Arn of target database or database cluster
+    #XXX: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-rds-dbproxy-authformat.html
+    db_proxy = aws_rds.DatabaseProxy(self, 'DBProxy',
+      proxy_target=aws_rds.ProxyTarget.from_cluster(db_cluster),
+      secrets=[db_secret],
+      vpc=vpc,
+      db_proxy_name='{}-proxy'.format(db_cluster_name),
+      idle_client_timeout=core.Duration.minutes(10),
+      max_connections_percent=90,
+      max_idle_connections_percent=10,
+      security_groups=[sg_use_mysql, sg_mysql_public_proxy],
+      vpc_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC)
     )
 
 
