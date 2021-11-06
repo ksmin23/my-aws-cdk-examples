c478f0189a63a70c1d124c5d9b6a6aa571b1ccc9
ksmin23 <sonagy23@gmail.com>
2021-11-06 18:18:50 +0900

    add ELK stack for Amazon OpenSearch

---
+++ kinesis-data-firehose/opskk-stack/README.md
@@ -0,0 +1,213 @@
+# OSKK (Amazon OpenSearch Service - Kinesis Data Firehose - Kibana) Stack
+
+![amazon-opskk-stack-arch](./amazon-opskk-stack-arch.svg)
+
+This is a collection of projects for Python development with CDK.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+This project is set up like a standard Python project.  The initialization
+process also creates a virtualenv within this project, stored under the .env
+directory.  To create the virtualenv it assumes that there is a `python3`
+(or `python` for Windows) executable in your path with access to the `venv`
+package. If for any reason the automatic creation of the virtualenv fails,
+you can create the virtualenv manually.
+
+To manually create a virtualenv on MacOS and Linux:
+
+```
+$ python3 -m venv .env
+```
+
+After the init process completes and the virtualenv is created, you can use the following
+step to activate your virtualenv.
+
+```
+$ source .env/bin/activate
+```
+
+If you are a Windows platform, you would activate the virtualenv like this:
+
+```
+% .env\Scripts\activate.bat
+```
+
+Once the virtualenv is activated, you can install the required dependencies.
+
+```
+$ pip install -r requirements.txt
+```
+
+At this point you can now synthesize the CloudFormation template for this code.
+
+<pre>
+(.venv) $ export CDK_DEFAULT_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
+(.venv) $ export CDK_DEFAULT_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+(.venv) $ cdk synth \
+              --parameters OpenSearchDomainName="<i>your-opensearch-domain-name</i>" \
+              --parameters SearchIndexName="<i>your-opensearch-index-name</i>" \
+              --parameters EC2KeyPairName="<i>your-ec2-key-pair-name</i>"
+</pre>
+
+Use `cdk deploy` command to create the stack shown above.
+
+<pre>
+(.venv) $ cdk deploy \
+              --parameters OpenSearchDomainName="<i>your-opensearch-domain-name</i>" \
+              --parameters SearchIndexName="<i>your-opensearch-index-name</i>" \
+              --parameters EC2KeyPairName="<i>your-ec2-key-pair-name</i>"
+</pre>
+
+To add additional dependencies, for example other CDK libraries, just add
+them to your `setup.py` file and rerun the `pip install -r requirements.txt`
+command.
+
+## Useful commands
+
+ * `cdk ls`          list all stacks in the app
+ * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk docs`        open CDK documentation
+
+Enjoy!
+
+## Test
+1. Checkout the code to generate sample data into Kinesis Data Firehose.
+
+    ```
+    $ python3 -m venv .env
+    $ source .env/bin/activate
+    (.env) $ ls -1
+    README.md
+    amazon-oskk-stack-arch.svg
+    app.py
+    cdk.context.json
+    cdk.json
+    requirements.txt
+    resources
+    test
+    (.env) $ ls test
+    gen_firehose_data.py
+    (.env) $
+    ```
+
+2. Run the test code. For example,
+   - AWS Region: `us-east-1`
+   - Kinesis Data Fireshose Stream Name: `retail-trans`
+
+    ```
+    (.env) $ python test/gen_firehose_data.py --region-name us-east-1 --stream-name retail-trans --count 10
+    (.env) $
+    ```
+
+## Remotely access your Amazon OpenSearch Cluster using SSH tunnel from local machine
+#### Access to your Amazon OpenSearch Dashboards with web browser
+1. To access the OpenSearch Cluster, add the ssh tunnel configuration to the ssh config file of the personal local PC as follows
+
+    <pre>
+    # OpenSearch Tunnel
+    Host osstunnel
+        HostName <i>EC2-Public-IP-of-Bastion-Host</i>
+        User ec2-user
+        IdentitiesOnly yes
+        IdentityFile <i>Path-to-SSH-Public-Key</i>
+        LocalForward 9200 <i>OpenSearch-Endpoint</i>:443
+    </pre>
+
+    ex)
+
+    ```
+    ~$ ls -1 .ssh/
+    config
+    my-ec2-key-pair.pem
+
+    ~$ tail .ssh/config
+    # OpenSearch Tunnel
+    Host opstunnel
+        HostName 214.132.71.219
+        User ec2-user
+        IdentitiesOnly yes
+        IdentityFile ~/.ssh/my-ec2-key-pair.pem
+        LocalForward 9200 vpc-search-domain-qvwlxanar255vswqna37p2l2cy.us-east-1.es.amazonaws.com:443
+
+    ~$
+    ```
+
+    You can find the bastion host's public ip address as running the commands like this:
+
+    <pre>
+    $ BASTION_HOST_ID=$(aws cloudformation describe-stacks --stack-name <i>your-clouformation-stack-name</i> | jq -r '.Stacks[0].Outputs | map(select(.OutputKey == "BastionHostBastionHostId")) | .[0].OutputValue')
+    $ aws ec2 describe-instances --instance-ids ${BASTION_HOST_ID} | jq -r '.Reservations[0].Instances[0].PublicIpAddress'
+    </pre>
+
+2. Run `ssh -N opstunnel` in Terminal.
+3. Connect to `https://localhost:9200/_dashboards/` in a web browser.
+4. Enter the master user and password that you set up when you created the Amazon OpenSearch Service endpoint.
+5. In the Welcome screen, click the toolbar icon to the left side of **Home** button. Choose **Stack Managerment**
+   ![ops-dashboards-sidebar-menu](./resources/ops-dashboards-sidebar-menu.png)
+6. After selecting **Advanced Settings** from the left sidebar menu, set **Timezone** for date formatting to `Etc/UTC`.
+   Since the log creation time of the test data is based on UTC, OpenSearch Dashboard’s Timezone is also set to UTC.
+   ![ops-dashboards-stack-management-advanced-setting.png](./resources/ops-dashboards-stack-management-advanced-setting.png)
+7. If you would like to access the OpenSearch Cluster in a termial, open another terminal window, and then run the following commands: (in here, <i>`your-cloudformation-stack-name`</i> is `OpensearchStack`)
+
+    <pre>
+    $ MASTER_USER_SECRET_ID=$(aws cloudformation describe-stacks --stack-name <i>your-clouformation-stack-name</i> | jq -r '.Stacks[0].Outputs | map(select(.OutputKey == "MasterUserSecretId")) | .[0].OutputValue')
+    $ export OPS_SECRETS=$(aws secretsmanager get-secret-value --secret-id ${MASTER_USER_SECRET_ID} | jq -r '.SecretString | fromjson | "\(.username):\(.password)"')
+    $ export OPS_DOMAIN=$(aws cloudformation describe-stacks --stack-name <i>your-clouformation-stack-name</i> | jq -r '.Stacks[0].Outputs | map(select(.OutputKey == "OpenSearchDomainEndpoint")) | .[0].OutputValue')
+    $ curl -XGET --insecure -u "${OPS_SECRETS}" https://localhost:9200/_cluster/health?pretty=true
+    $ curl -XGET --insecure -u "${OPS_SECRETS}" https://localhost:9200/_cat/nodes?v
+    $ curl -XGET --insecure -u "${OPS_SECRETS}" https://localhost:9200/_nodes/stats?pretty=true
+    </pre>
+
+#### Enable Kinesis Data Firehose to ingest records into Amazon OpenSearch
+Kinesis Data Firehose uses the delivery role to sign HTTP (Signature Version 4) requests before sending the data to the Amazon OpenSearch Service endpoint.
+You manage Amazon OpenSearch Service fine-grained access control permissions using roles, users, and mappings.
+This section describes how to create roles and set permissions for Kinesis Data Firehose.
+
+Complete the following steps:
+
+1. Navigate to Kibana (you can find the URL on the Amazon OpenSearch Service console).
+2. Enter the master user and password that you set up when you created the Amazon OpenSearch Service endpoint.
+3. Under **Security**, choose **Roles**.
+4. Choose **Create role**.
+5. Name your role; for example, `firehose-role`.
+6. For cluster permissions, add `cluster_composite_ops` and `cluster_monitor`.
+7. Under **Index permissions**, choose **Index Patterns** and enter <i>index-name*</i>; for example, `retail-trans*`.
+8. Under **Permissions**, add three action groups: `crud`, `create_index`, and `manage`.
+9. Choose **Create**.
+
+In the next step, you map the IAM role that Kinesis Data Firehose uses to the role you just created.
+
+10. Under **Security**, choose **Roles**.
+11. Choose the role you just created (`firehose-role`). You need to modify the Users associated with the `firehose-role` role.
+    ![ops-security-roles](./resources/ops-security-roles.png)
+12. Choose the **Mapped users** tab.
+    ![ops-role-mappings](./resources/ops-role-mappings.png)
+13. Choose **Manage mapping** and under **Backend roles**,
+14. For **Backend Roles**, enter the IAM ARN of the role Kinesis Data Firehose uses:
+    `arn:aws:iam::123456789012:role/firehose_stream_role_name`.
+    ![ops-entries-for-firehose_role](./resources/ops-entries-for-firehose_role.png)
+15. Choose **Map**.
+
+**Note**: After OpenSearch Role mapping for Kinesis Data Firehose, you would not be supposed to meet a data delivery failure with Kinesis Data Firehose like this:
+
+<pre>
+"errorMessage": "Error received from Elasticsearch cluster. {\"error\":{\"root_cause\":[{\"type\":\"security_exception\",\"reason\":\"no permissions for [indices:data/write/bulk] and User [name=arn:aws:iam::123456789012:role/KinesisFirehoseServiceRole-<i>firehose_stream_name</i>-<i>region_name</i>, backend_roles=[arn:aws:iam::123456789012:role/KinesisFirehoseServiceRole-<i>firehose_stream_name</i>-<i>region_name</i>], requestedTenant=null]\"}],\"type\":\"security_exception\",\"reason\":\"no permissions for [indices:data/write/bulk] and User [name=arn:aws:iam::123456789012:role/KinesisFirehoseServiceRole-<i>firehose_stream_name</i>-<i>region_name</i>, backend_roles=[arn:aws:iam::123456789012:role/KinesisFirehoseServiceRole-<i>firehose_stream_name</i>-<i>region_name</i>], requestedTenant=null]\"},\"status\":403}",
+</pre>
+
+## Considerations
+Check the following questions before using Kinesis Data Firehose to ingest documents into Amazon OpenSearch Service
+- [ ] Need to change `_type`?
+- [ ] Should update documents in Amazon OpenSearch Service?
+- [ ] Want to use custome `_id` instead of auto generated `_id` by OpenSearch?
+
+## References
+1. [Windows SSH / Tunnel for Kibana Instructions - Amazon Elasticsearch Service](https://search-sa-log-solutions.s3-us-east-2.amazonaws.com/logstash/docs/Kibana_Proxy_SSH_Tunneling_Windows.pdf)
+2. [Use an SSH Tunnel to access Kibana within an AWS VPC with PuTTy on Windows](https://amazonmsk-labs.workshop.aws/en/mskkdaflinklab/createesdashboard.html)
+3. [OpenSearch Popular APIs](https://opensearch.org/docs/latest/opensearch/popular-api/)
+4. [Why am I experiencing a data delivery failure with Kinesis Data Firehose?](https://aws.amazon.com/premiumsupport/knowledge-center/kinesis-data-firehose-delivery-failure/)
+5. [Ingest streaming data into Amazon Elasticsearch Service within the privacy of your VPC with Amazon Kinesis Data Firehose](https://aws.amazon.com/blogs/big-data/ingest-streaming-data-into-amazon-elasticsearch-service-within-the-privacy-of-your-vpc-with-amazon-kinesis-data-firehose/)
+6. [How do I troubleshoot CloudWatch Logs so that it streams to my Amazon OpenSearch Service domain?](https://aws.amazon.com/premiumsupport/knowledge-center/opensearch-troubleshoot-cloudwatch-logs/)
+7. [Build a Log Analytics Solution on AWS](https://aws.amazon.com/getting-started/hands-on/build-log-analytics-solution/)
+8. [Hands-on guide to Build a Log Analytics Solution on AWS](https://d1.awsstatic.com/Projects/P4113850/aws-projects_build-log-analytics-solution-on-aws.pdf)
---
+++ kinesis-data-firehose/opskk-stack/app.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import random
+import string
+
+from aws_cdk import (
+  core as cdk,
+  aws_ec2,
+  aws_iam,
+  aws_s3 as s3,
+  aws_kinesisfirehose,
+  aws_opensearchservice,
+  aws_secretsmanager
+)
+
+random.seed(47)
+
+class OPSKKStack(cdk.Stack):
+
+  def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
+    super().__init__(scope, id, **kwargs)
+
+    OPENSEARCH_DOMAIN_NAME = cdk.CfnParameter(self, 'OpenSearchDomainName',
+      type='String',
+      description='Amazon OpenSearch Service domain name',
+      default='opensearch-{}'.format(''.join(random.sample((string.ascii_letters), k=5))),
+      allowed_pattern='[a-z]+[A-Za-z0-9\-]+'
+    )
+
+    OPENSEARCH_INDEX_NAME = cdk.CfnParameter(self, 'SearchIndexName',
+      type='String',
+      description='Amazon OpenSearch Service index name'
+    )
+
+    EC2_KEY_PAIR_NAME = cdk.CfnParameter(self, 'EC2KeyPairName',
+      type='String',
+      description='Amazon EC2 Instance KeyPair name'
+    )
+
+    # vpc_name = self.node.try_get_context("vpc_name")
+    # vpc = aws_ec2.Vpc.from_lookup(self, "ExistingVPC",
+    #   is_default=True,
+    #   vpc_name=vpc_name)
+    #
+    vpc = aws_ec2.Vpc(self, "EKKStackVPC",
+      max_azs=2,
+      gateway_endpoints={
+        "S3": aws_ec2.GatewayVpcEndpointOptions(
+          service=aws_ec2.GatewayVpcEndpointAwsService.S3
+        )
+      }
+    )
+
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/InstanceClass.html
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/InstanceSize.html#aws_cdk.aws_ec2.InstanceSize
+    ec2_instance_type = aws_ec2.InstanceType.of(aws_ec2.InstanceClass.BURSTABLE3, aws_ec2.InstanceSize.MEDIUM)
+
+    sg_bastion_host = aws_ec2.SecurityGroup(self, "BastionHostSG",
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for an bastion host',
+      security_group_name='bastion-host-sg'
+    )
+    cdk.Tags.of(sg_bastion_host).add('Name', 'bastion-host-sg')
+
+    #TODO: SHOULD restrict IP range allowed to ssh acces
+    sg_bastion_host.add_ingress_rule(peer=aws_ec2.Peer.ipv4("0.0.0.0/0"), connection=aws_ec2.Port.tcp(22), description='SSH access')
+
+    bastion_host = aws_ec2.Instance(self, "BastionHost",
+      vpc=vpc,
+      instance_type=ec2_instance_type,
+      machine_image=aws_ec2.MachineImage.latest_amazon_linux(),
+      vpc_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC),
+      security_group=sg_bastion_host,
+      key_name=EC2_KEY_PAIR_NAME.value_as_string
+    )
+
+    sg_use_opensearch = aws_ec2.SecurityGroup(self, "OpenSearchClientSG",
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for an opensearch client',
+      security_group_name='use-opensearch-cluster-sg'
+    )
+    cdk.Tags.of(sg_use_opensearch).add('Name', 'use-opensearch-cluster-sg')
+
+    sg_opensearch_cluster = aws_ec2.SecurityGroup(self, "OpenSearchSG",
+      vpc=vpc,
+      allow_all_outbound=True,
+      description='security group for an opensearch cluster',
+      security_group_name='opensearch-cluster-sg'
+    )
+    cdk.Tags.of(sg_opensearch_cluster).add('Name', 'opensearch-cluster-sg')
+
+    sg_opensearch_cluster.add_ingress_rule(peer=sg_opensearch_cluster, connection=aws_ec2.Port.all_tcp(), description='opensearch-cluster-sg')
+
+    sg_opensearch_cluster.add_ingress_rule(peer=sg_use_opensearch, connection=aws_ec2.Port.tcp(443), description='use-opensearch-cluster-sg')
+    sg_opensearch_cluster.add_ingress_rule(peer=sg_use_opensearch, connection=aws_ec2.Port.tcp_range(9200, 9300), description='use-opensearch-cluster-sg')
+
+    sg_opensearch_cluster.add_ingress_rule(peer=sg_bastion_host, connection=aws_ec2.Port.tcp(443), description='bastion-host-sg')
+    sg_opensearch_cluster.add_ingress_rule(peer=sg_bastion_host, connection=aws_ec2.Port.tcp_range(9200, 9300), description='bastion-host-sg')
+
+    master_user_secret = aws_secretsmanager.Secret(self, "OpenSearchMasterUserSecret",
+      generate_secret_string=aws_secretsmanager.SecretStringGenerator(
+        secret_string_template=json.dumps({"username": "admin"}),
+        generate_string_key="password",
+        # Master password must be at least 8 characters long and contain at least one uppercase letter,
+        # one lowercase letter, one number, and one special character.
+        password_length=8
+      )
+    )
+
+    #XXX: aws cdk elastsearch example - https://github.com/aws/aws-cdk/issues/2873
+    # You should camelCase the property names instead of PascalCase
+    opensearch_domain = aws_opensearchservice.Domain(self, "OpenSearch",
+      domain_name=OPENSEARCH_DOMAIN_NAME.value_as_string,
+      version=aws_opensearchservice.EngineVersion.OPENSEARCH_1_0,
+      #XXX: You cannot use graviton instances with non-graviton instances.
+      # Use graviton instances as data nodes or use non-graviton instances as master nodes.
+      capacity={
+        "master_nodes": 3,
+        "master_node_instance_type": "r6g.large.search",
+        "data_nodes": 3,
+        "data_node_instance_type": "r6g.large.search"
+      },
+      ebs={
+        "volume_size": 10,
+        "volume_type": aws_ec2.EbsDeviceVolumeType.GP2
+      },
+      #XXX: az_count must be equal to vpc subnets count.
+      zone_awareness={
+        "availability_zone_count": 3
+      },
+      logging={
+        "slow_search_log_enabled": True,
+        "app_log_enabled": True,
+        "slow_index_log_enabled": True
+      },
+      fine_grained_access_control=aws_opensearchservice.AdvancedSecurityOptions(
+        master_user_name=master_user_secret.secret_value_from_json("username").to_string(),
+        master_user_password=master_user_secret.secret_value_from_json("password")
+      ),
+      # Enforce HTTPS is required when fine-grained access control is enabled.
+      enforce_https=True,
+      # Node-to-node encryption is required when fine-grained access control is enabled
+      node_to_node_encryption=True,
+      # Encryption-at-rest is required when fine-grained access control is enabled.
+      encryption_at_rest={
+        "enabled": True
+      },
+      use_unsigned_basic_auth=True,
+      security_groups=[sg_opensearch_cluster],
+      automated_snapshot_start_hour=17, # 2 AM (GTM+9)
+      vpc=vpc,
+      vpc_subnets=[aws_ec2.SubnetSelection(one_per_az=True, subnet_type=aws_ec2.SubnetType.PRIVATE)],
+      removal_policy=cdk.RemovalPolicy.DESTROY # default: cdk.RemovalPolicy.RETAIN
+    )
+    cdk.Tags.of(opensearch_domain).add('Name', f'{OPENSEARCH_DOMAIN_NAME.value_as_string}')
+
+    S3_BUCKET_SUFFIX = ''.join(random.sample((string.ascii_lowercase + string.digits), k=7))
+    s3_bucket = s3.Bucket(self, "s3bucket",
+      removal_policy=cdk.RemovalPolicy.DESTROY, #XXX: Default: core.RemovalPolicy.RETAIN - The bucket will be orphaned
+      bucket_name="opskk-stack-{region}-{suffix}".format(
+        region=cdk.Aws.REGION, suffix=S3_BUCKET_SUFFIX))
+
+    firehose_role_policy_doc = aws_iam.PolicyDocument()
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "effect": aws_iam.Effect.ALLOW,
+      "resources": [s3_bucket.bucket_arn, "{}/*".format(s3_bucket.bucket_arn)],
+      "actions": ["s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"]
+    }))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["ec2:DescribeVpcs",
+        "ec2:DescribeVpcAttribute",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:CreateNetworkInterface",
+        "ec2:CreateNetworkInterfacePermission",
+        "ec2:DeleteNetworkInterface"]
+    ))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=[opensearch_domain.domain_arn, "{}/*".format(opensearch_domain.domain_arn)],
+      actions=["es:DescribeElasticsearchDomain",
+        "es:DescribeElasticsearchDomains",
+        "es:DescribeElasticsearchDomainConfig",
+        "es:ESHttpPost",
+        "es:ESHttpPut"]
+    ))
+
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      #XXX: https://aws.amazon.com/premiumsupport/knowledge-center/kinesis-data-firehose-delivery-failure/
+      resources=[
+        opensearch_domain.domain_arn,
+        f"{opensearch_domain.domain_arn}/_all/_settings",
+        f"{opensearch_domain.domain_arn}/_cluster/stats",
+        f"{opensearch_domain.domain_arn}/{OPENSEARCH_INDEX_NAME.value_as_string}*/_mapping/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%",
+        f"{opensearch_domain.domain_arn}/_nodes",
+        f"{opensearch_domain.domain_arn}/_nodes/stats",
+        f"{opensearch_domain.domain_arn}/_nodes/*/stats",
+        f"{opensearch_domain.domain_arn}/_stats",
+        f"{opensearch_domain.domain_arn}/{OPENSEARCH_INDEX_NAME.value_as_string}*/_stats"
+      ],
+      actions=["es:ESHttpGet"]
+    ))
+
+    firehose_log_group_name = f"/aws/kinesisfirehose/{OPENSEARCH_INDEX_NAME.value_as_string}"
+    firehose_role_policy_doc.add_statements(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      #XXX: The ARN will be formatted as follows:
+      # arn:{partition}:{service}:{region}:{account}:{resource}{sep}}{resource-name}
+      resources=[self.format_arn(service="logs", resource="log-group",
+        resource_name="{}:log-stream:*".format(firehose_log_group_name), sep=":")],
+      actions=["logs:PutLogEvents"]
+    ))
+
+    firehose_role = aws_iam.Role(self, "KinesisFirehoseServiceRole",
+      role_name=f"KinesisFirehoseServiceRole-{OPENSEARCH_INDEX_NAME.value_as_string}-{cdk.Aws.REGION}",
+      assumed_by=aws_iam.ServicePrincipal("firehose.amazonaws.com"),
+      #XXX: use inline_policies to work around https://github.com/aws/aws-cdk/issues/5221
+      inline_policies={
+        "firehose_role_policy": firehose_role_policy_doc
+      }
+    )
+
+    opensearch_dest_vpc_config = aws_kinesisfirehose.CfnDeliveryStream.VpcConfigurationProperty(
+      role_arn=firehose_role.role_arn,
+      security_group_ids=[sg_use_opensearch.security_group_id],
+      subnet_ids=vpc.select_subnets(subnet_type=aws_ec2.SubnetType.PRIVATE).subnet_ids
+    )
+
+    opensearch_dest_config = aws_kinesisfirehose.CfnDeliveryStream.ElasticsearchDestinationConfigurationProperty(
+      index_name=OPENSEARCH_INDEX_NAME.value_as_string,
+      role_arn=firehose_role.role_arn,
+      s3_configuration={
+        "bucketArn": s3_bucket.bucket_arn,
+        "bufferingHints": {
+          "intervalInSeconds": 60,
+          "sizeInMBs": 1
+        },
+        "cloudWatchLoggingOptions": {
+          "enabled": True,
+          "logGroupName": firehose_log_group_name,
+          "logStreamName": "S3Backup"
+        },
+        "compressionFormat": "UNCOMPRESSED", # [GZIP | HADOOP_SNAPPY | Snappy | UNCOMPRESSED | ZIP]
+        # Kinesis Data Firehose automatically appends the “YYYY/MM/dd/HH/” UTC prefix to delivered S3 files. You can also specify
+        # an extra prefix in front of the time format and add "/" to the end to have it appear as a folder in the S3 console.
+        "prefix": f"{OPENSEARCH_INDEX_NAME.value_as_string}/",
+        "roleArn": firehose_role.role_arn
+      },
+      buffering_hints={
+        "intervalInSeconds": 60,
+        "sizeInMBs": 1
+      },
+      cloud_watch_logging_options={
+        "enabled": True,
+        "logGroupName": firehose_log_group_name,
+        "logStreamName": "ElasticsearchDelivery"
+      },
+      domain_arn=opensearch_domain.domain_arn,
+      index_rotation_period="NoRotation", # [NoRotation | OneDay | OneHour | OneMonth | OneWeek]
+      retry_options={
+        "durationInSeconds": 60
+      },
+      s3_backup_mode="FailedDocumentsOnly", # [AllDocuments | FailedDocumentsOnly]
+      vpc_configuration=opensearch_dest_vpc_config
+    )
+
+    firehose_to_ops_delivery_stream = aws_kinesisfirehose.CfnDeliveryStream(self, "KinesisFirehoseToES",
+      delivery_stream_name=OPENSEARCH_INDEX_NAME.value_as_string,
+      delivery_stream_type="DirectPut",
+      elasticsearch_destination_configuration=opensearch_dest_config,
+      tags=[{"key": "Name", "value": OPENSEARCH_INDEX_NAME.value_as_string}]
+    )
+
+    cdk.CfnOutput(self, 'BastionHostId', value=bastion_host.instance_id, export_name='BastionHostId')
+    cdk.CfnOutput(self, 'OpenSearchDomainEndpoint', value=opensearch_domain.domain_endpoint, export_name='OpenSearchDomainEndpoint')
+    cdk.CfnOutput(self, 'OpenSearchDashboardsURL', value=f"{opensearch_domain.domain_endpoint}/_dashboards/", export_name='OpenSearchDashboardsURL')
+    cdk.CfnOutput(self, 'MasterUserSecretId', value=master_user_secret.secret_name, export_name='MasterUserSecretId')
+    cdk.CfnOutput(self, '{}_S3DestBucket'.format(self.stack_name), value=s3_bucket.bucket_name, export_name='S3DestBucket')
+    cdk.CfnOutput(self, 'FirehoseRoleArn', value=firehose_role.role_arn, export_name='FirehoseRoleArn')
+
+
+app = cdk.App()
+OPSKKStack(app, "OpenSearchFirehoseKibanaStack", env=cdk.Environment(
+  account=os.environ["CDK_DEFAULT_ACCOUNT"],
+  region=os.environ["CDK_DEFAULT_REGION"]))
+
+app.synth()
