556ea91e95e8ca9fd32c186b09bcddd77a5945ff
ksmin23 <sonagy23@gmail.com>
2022-06-08 00:34:26 +0900

    create Cloud9 with owner name

--- cloud9/app.py
+++ cloud9/app.py
@@ -6,7 +6,9 @@ import aws_cdk as cdk
 from aws_cdk import (
   Stack,
   aws_ec2,
-  aws_cloud9_alpha as cloud9
+  aws_iam,
+  #aws_cloud9_alpha as cloud9
+  aws_cloud9 as cloud9
 )
 from constructs import Construct
 
@@ -14,6 +16,11 @@ class Cloud9Stack(Stack):
 
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
+
+    C9_OWNER_NAME = cdk.CfnParameter(self, 'Cloud9OwnerName',
+      type='String',
+      description='Cloud9 Owner name'
+    )
 
     #XXX: For createing Amazon MWAA in the existing VPC,
     # remove comments from the below codes and
@@ -36,15 +43,23 @@ class Cloud9Stack(Stack):
     #     )
     #   }
     # )
-    c9env = cloud9.Ec2Environment(self, "Cloud9Env",
-      vpc=vpc,
-      subnet_selection=aws_ec2.SubnetSelection(
-        subnet_type=aws_ec2.SubnetType.PUBLIC
-      ),
-      instance_type=aws_ec2.InstanceType("m5.large")
-    )
 
-    cdk.CfnOutput(self, "C9URL", value=c9env.ide_url)
+    #XXX: AWS Cloud9 CloudFormation Guide
+    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cloud9-environmentec2.html
+    c9owner = aws_iam.User.from_user_name(self, "Cloud9Owner",
+      C9_OWNER_NAME.value_as_string)
+
+    c9env = cloud9.CfnEnvironmentEC2(self, "Cloud9Env",
+      instance_type="m5.large",
+
+      # the properties below are optional
+      automatic_stop_time_minutes=30,
+      connection_type="CONNECT_SSH", # [CONNECT_SSH, CONNECT_SSM]
+      #image_id="amazonlinux-2-x86_64", # "amazonlinux-1-x86_64" | "amazonlinux-2-x86_64" | "ubuntu-18.04-x86_64"
+      name="c9env",
+      owner_arn=c9owner.user_arn,
+      subnet_id=vpc.select_subnets(subnet_type=aws_ec2.SubnetType.PUBLIC).subnet_ids[0]
+    )
 
 
 app = cdk.App()
