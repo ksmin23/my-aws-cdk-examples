210aeb568bf0e3e787ab4ab2e2d617f6b03ebf4e
ksmin23 <sonagy23@gmail.com>
2022-08-14 01:25:33 +0900

    use mimesis instead of faker to generate sample data

--- glue/src/utils/gen_fake_cdc_parquet.py
+++ glue/src/utils/gen_fake_cdc_parquet.py
@@ -8,42 +8,119 @@ import collections
 import datetime
 import random
 
-from faker import Faker
 import pandas as pd
 
+from mimesis.locales import Locale
+from mimesis.schema import Field, Schema
+
 random.seed(47)
+
+DEPARTMENTS = [
+  'FC',
+  'Finance',
+  'IT',
+  'Laws',
+  'Manufacturing',
+  'Marketing',
+  'Purchasing',
+  'R&D',
+  'Sales',
+  'Security'
+]
+
+COLUMNS_INFO = collections.OrderedDict([
+  ('Op', 'N'),
+  ('emp_no', 'KEY'),
+  ('name', 'KEY'),
+  ('department', 'U'),
+  ('city', 'U'),
+  ('salary', 'U'),
+  ('m_time', 'N')
+])
+
+TODAY = datetime.date.today()
+
+
+def gen_df(schema, n, time_col='m_time'):
+  records = schema.create(iterations=n)
+  res_df = pd.DataFrame.from_records(records)
+  res_df[time_col] = res_df[time_col].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ'))
+  return res_df
+
+
+def gen_updated_cdc_df(sample_df, locale=Locale.EN):
+  start_date = TODAY - datetime.timedelta(days=365*2)
+  end_date = TODAY - datetime.timedelta(days=365*2)
+
+  _ = Field(locale=locale)
+  updated_schema = Schema(schema=lambda: {
+    "Op": 'U',
+    "department": _("choice", items=DEPARTMENTS),
+    "city": _("city"),
+    "salary": _("integer_number", start=50000, end=100000),
+    "m_time": _("timestamp", posix=False, start=start_date.year, end=end_date.year)
+  })
+
+  num_samples = len(sample_df)
+  updated_df = gen_df(updated_schema, num_samples)
+
+  NON_UPDATABLE_COLUMNS = [k for k, v in COLUMNS_INFO.items() if v == 'KEY']
+  old_df = sample_df[NON_UPDATABLE_COLUMNS].reset_index(drop=True)
+  columns = [v for v in COLUMNS_INFO.keys()]
+  res_df = pd.concat([updated_df, old_df], axis=1)[columns]
+  return res_df
+
+
+def gen_deleted_cdc_df(sample_df, locale=Locale.EN):
+  start_date = TODAY - datetime.timedelta(days=365)
+  end_date = TODAY - datetime.timedelta(days=365)
+
+  _ = Field(locale=locale)
+  deleted_schema = Schema(schema=lambda: {
+    "Op": 'D',
+    "m_time": _("timestamp", posix=False, start=start_date.year, end=end_date.year)
+  })
+
+  num_samples = len(sample_df)
+  deleted_df = gen_df(deleted_schema, num_samples)
+
+  NON_DELETED_COLUMNS = [k for k, v in COLUMNS_INFO.items() if v in ('KEY', 'U')]
+  old_df = sample_df[NON_DELETED_COLUMNS].reset_index(drop=True)
+  columns = [v for v in COLUMNS_INFO.keys()]
+  res_df = pd.concat([deleted_df, old_df], axis=1)[columns]
+  return res_df
+
+
+def gen_inserted_cdc_df(insert_schema, n):
+  res_df = gen_df(insert_schema, n)
+  res_df['m_time'] = res_df['m_time'].apply(lambda x: x + datetime.timedelta(days=365*3))
+  return res_df
+
 
 def main():
   parser = argparse.ArgumentParser()
 
   parser.add_argument('--type', default='FULL_LOAD_AND_CDC', choices=['FULL_LOAD', 'FULL_LOAD_AND_CDC'])
-  parser.add_argument('--max-count', default=15, type=int, choices=range(1, 20), help='The max number of records to put. [1, 20)')
+  parser.add_argument('--max-count', default=15, type=int, choices=range(10, 20), help='The max number of records to put. [10, 20)')
   parser.add_argument('--dry-run', action='store_true')
 
   options = parser.parse_args()
 
-  fake = Faker()
+  start_date = TODAY - datetime.timedelta(days=365*4)
+  end_date = TODAY - datetime.timedelta(days=365*3)
 
-  fake.set_arguments('append_only', {'elements': ['I']})
-  fake.set_arguments('emp_no', {'digits': 3, 'fix_len': True})
-  fake.set_arguments('department', {'elements': ['Finance', 'IT', 'Manufacturing', 'Purchasing', 'Sales']})
-  fake.set_arguments('city', {'elements': ['Chicago', 'NY', 'Seoul', 'SFO', 'Tokyo']})
-  fake.set_arguments('salary', {'digits': 5, 'fix_len': True})
-  fake.set_arguments('end_mtime', {'end_datetime': datetime.date.today(), 'timespec': 'milliseconds'})
+  _ = Field(locale=Locale.EN)
+  insert_schema = Schema(schema=lambda: {
+    "Op": 'I',
+    "emp_no": _("increment", accumulator="emp_no"),
+    "name": _("full_name"),
+    "department": _("choice", items=DEPARTMENTS), 
+    "city": _("city"),
+    "salary": _("integer_number", start=1000, end=100000),
+    "m_time": _("timestamp", posix=False, start=start_date.year, end=end_date.year)
+  })
 
-  DATA_COLUMNS = collections.OrderedDict([
-    ('Op', 'random_element:append_only'),
-    ('emp_no', 'random_number:emp_no'),
-    ('name', 'first_name'),
-    ('department', 'random_element:department'),
-    ('city', 'random_element:city'),
-    ('salary', 'random_number:salary'),
-    ('m_time', 'iso8601:end_mtime')
-  ])
-
-  full_load_records = fake.json(data_columns=DATA_COLUMNS, num_rows=options.max_count)
-  full_load_df = pd.read_json(full_load_records, orient='records')
-  full_load_df = full_load_df.sort_values(by=['emp_no']).reset_index(drop=True)
+  full_load_df = gen_df(insert_schema, options.max_count)
 
   print('\n[full-load data]', file=sys.stderr)
   print(full_load_df, file=sys.stderr)
@@ -54,58 +131,20 @@ def main():
   if options.type == 'FULL_LOAD':
     return
 
-  sample_df = full_load_df.sample(frac=0.3, random_state=47)
-  num_samples = len(sample_df)
+  num_inserted_records = max(1, int(options.max_count*0.3))
+  inserted_cdc_df = gen_inserted_cdc_df(insert_schema, num_inserted_records)
 
-  new_mtime_list = []
-  base_dt = datetime.datetime.utcnow() - datetime.timedelta(days=20)
-  for _ in range(num_samples):
-    base_dt += datetime.timedelta(hours=random.randint(1, 3), minutes=random.randint(1, 10))
-    new_mtime_list.append(base_dt.isoformat(timespec='milliseconds'))
+  updated_sample_df = full_load_df.sample(frac=0.5, random_state=23)
+  updated_cdc_df = gen_updated_cdc_df(updated_sample_df, locale=Locale.EN)
 
-  fake.set_arguments('update_delete', {'elements': collections.OrderedDict([('U', 0.7), ('D', 0.3)])})
-  fake.set_arguments('new_city', {'elements': ['Lisbon', 'Mumbai', 'Pargue', 'Seattle', 'Sydney', 'Taipei']})
-  fake.set_arguments('new_department', {'elements': ['FC', 'Laws', 'Marketing', 'R&D', 'Security']})
-  fake.set_arguments('new_mtime', {'elements': new_mtime_list})
+  twice_updated_sample_df = updated_sample_df.sample(frac=0.3, random_state=37)
+  twice_updated_cdc_df = gen_updated_cdc_df(twice_updated_sample_df, locale=Locale.EN)
 
-  UPDATED_DATA_COLUMNS = {
-    'Op': 'random_element:update_delete',
-    'department': 'random_element:new_department',
-    'city': 'random_element:new_city',
-    'salary': 'random_number:salary',
-    'm_time': 'random_element:new_mtime'
-  }
+  deleted_sample_df = updated_sample_df.sample(frac=0.2, random_state=47)
+  deleted_cdc_df = gen_deleted_cdc_df(deleted_sample_df, locale=Locale.EN)
 
-  updated_or_deleted_records = fake.json(data_columns=UPDATED_DATA_COLUMNS, num_rows=num_samples)
-  updated_or_deleted_df = pd.read_json(updated_or_deleted_records, orient='records')
-
-  RENAME_COLUMNS = collections.OrderedDict([
-    ('Op', 'Op'),
-    ('emp_no', 'emp_no'),
-    ('name', 'name'),
-    ('department', 'department_2'),
-    ('city', 'city_2'),
-    ('salary', 'salary_2'),
-    ('m_time', 'm_time')
-  ])
-
-  emp_details_df = sample_df[['emp_no', 'name', 'department', 'city', 'salary']].rename(columns=RENAME_COLUMNS).reset_index(drop=True)
-  merged_cdc_df = pd.concat([updated_or_deleted_df, emp_details_df], axis=1)
-
-  DELETED_REC_COLUMNS = [v for v in RENAME_COLUMNS.values()]
-  deleted_cdc_df = merged_cdc_df[merged_cdc_df.Op == 'D'][DELETED_REC_COLUMNS].rename(columns={v: k for k, v in RENAME_COLUMNS.items()}).reset_index(drop=True)
-
-  UPDATED_REC_COLUMNS = [v for v in RENAME_COLUMNS.keys()]
-  updated_cdc_df = merged_cdc_df[merged_cdc_df.Op == 'U'][UPDATED_REC_COLUMNS].reset_index(drop=True)
-
-  fake.set_arguments('new_emp_no', {'digits': 4, 'fix_len': True})
-
-  DATA_COLUMNS['Op'] = 'random_element:append_only'
-  DATA_COLUMNS['emp_no'] = 'random_number:new_emp_no'
-
-  inserted_records = fake.json(data_columns=DATA_COLUMNS, num_rows=max(1, int(options.max_count*0.3)))
-  inserted_df = pd.read_json(inserted_records, orient='records')
-  cdc_df = pd.concat([deleted_cdc_df, updated_cdc_df, inserted_df]).sample(frac=1, random_state=47).reset_index(drop=True)
+  cdc_df = pd.concat([inserted_cdc_df, deleted_cdc_df,
+    updated_cdc_df, twice_updated_cdc_df]).sample(frac=1, random_state=47).sort_values(by=['m_time']).reset_index(drop=True)
 
   print('\n[cdc data]', file=sys.stderr)
   print(cdc_df, file=sys.stderr)
@@ -116,4 +155,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
