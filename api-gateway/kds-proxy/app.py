43bdf699b35dbf453d9618ac450f120e21b44e88
ksmin23 <sonagy23@gmail.com>
2022-08-28 02:13:02 +0900

    add an api to list Kinesis streams by using the API Gateway

---
+++ api-gateway/kds-proxy/app.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+import os
+import random
+import string
+
+import aws_cdk as cdk
+
+from aws_cdk import (
+  Duration,
+  Stack,
+  aws_apigateway,
+  aws_ec2,
+  aws_iam,
+  aws_kinesis,
+)
+from constructs import Construct
+
+random.seed(31)
+
+class KdsProxyStack(Stack):
+
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    KINESIS_STREAM_NAME = cdk.CfnParameter(self, 'KinesisStreamName',
+      type='String',
+      description='kinesis data stream name',
+      default='PUT-Firehose-{}'.format(''.join(random.sample((string.ascii_letters), k=5)))
+    )
+
+    #XXX: For creating this CDK Stack in the existing VPC,
+    # remove comments from the below codes and
+    # comments out vpc = aws_ec2.Vpc(..) codes,
+    # then pass -c vpc_name=your-existing-vpc to cdk command
+    # for example,
+    # cdk -c vpc_name=your-existing-vpc syth
+    #
+    vpc_name = self.node.try_get_context("vpc_name")
+    vpc = aws_ec2.Vpc.from_lookup(self, "ExistingVPC",
+      is_default=True,
+      vpc_name=vpc_name)
+
+    #XXX: To use more than 2 AZs, be sure to specify the account and region on your stack.
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
+    # vpc = aws_ec2.Vpc(self, "EmrStudioVPC",
+    #   max_azs=2,
+    #   gateway_endpoints={
+    #     "S3": aws_ec2.GatewayVpcEndpointOptions(
+    #       service=aws_ec2.GatewayVpcEndpointAwsService.S3
+    #     )
+    #   }
+    # )
+
+    source_kinesis_stream = aws_kinesis.Stream(self, "SourceKinesisStreams",
+      retention_period=Duration.hours(24),
+      stream_mode=aws_kinesis.StreamMode.ON_DEMAND, 
+      stream_name=KINESIS_STREAM_NAME.value_as_string)
+
+    apigw_kds_role = aws_iam.Role(self, "APIGatewayRoleToAccessKinesisDataStreams",
+      role_name='APIGatewayRoleToAccessKinesisDataStreams',
+      assumed_by=aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
+      managed_policies=[
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonKinesisReadOnlyAccess'),
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonKinesisFullAccess')
+      ]
+    )
+
+    kds_proxy_api = aws_apigateway.RestApi(self, "KdsProxyAPI",
+      rest_api_name="kds-proxy",
+      description="An Amazon API Gateway REST API that integrated with an Amazon Kinesis Data Streams.",
+      endpoint_types=[aws_apigateway.EndpointType.REGIONAL],
+      default_cors_preflight_options={
+        "allow_origins": aws_apigateway.Cors.ALL_ORIGINS
+      },
+      deploy=True,
+      deploy_options=aws_apigateway.StageOptions(stage_name="v1"),
+      endpoint_export_name="KdsProxyAPIEndpoint"
+    )
+
+    apigw_error_responses = [
+      aws_apigateway.IntegrationResponse(status_code="400", selection_pattern="4\d{2}"),
+      aws_apigateway.IntegrationResponse(status_code="500", selection_pattern="5\d{2}")
+    ]
+
+    apigw_ok_responses = [
+      aws_apigateway.IntegrationResponse(
+        status_code="200"
+      )
+    ]
+
+    streams_resource = kds_proxy_api.root.add_resource("streams")
+
+    list_streams_options = aws_apigateway.IntegrationOptions(
+      credentials_role=apigw_kds_role,
+      integration_responses=[
+        aws_apigateway.IntegrationResponse(
+          status_code="200"
+        )
+      ],
+      request_templates={
+        'application/json': '{}'
+      },
+      passthrough_behavior=aws_apigateway.PassthroughBehavior.WHEN_NO_TEMPLATES
+    )
+
+    list_streams_integration = aws_apigateway.AwsIntegration(
+      service='kinesis',
+      action='ListStreams',
+      integration_http_method='POST',
+      options=list_streams_options
+    )
+
+    streams_resource.add_method("GET", list_streams_integration,
+      method_responses=[aws_apigateway.MethodResponse(status_code='200')])
+
+    cdk.CfnOutput(self, '{}_KinesisDataStreamName'.format(self.stack_name), 
+      value=source_kinesis_stream.stream_name, export_name='KinesisDataStreamName')
+
+
+app = cdk.App()
+KdsProxyStack(app, "ApiGwKdsProxyStack", env=cdk.Environment(
+  account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+  region=os.getenv('CDK_DEFAULT_REGION')))
+
+app.synth()
