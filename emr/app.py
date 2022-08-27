c2bd9c9edb5b59910174f74b8d60ea30030dfb38
ksmin23 <sonagy23@gmail.com>
2022-08-27 21:18:33 +0900

    update copy jar bootstrap action in emr

--- emr/app.py
+++ emr/app.py
@@ -118,7 +118,13 @@ class EmrStack(Stack):
         args=[
           "bash",
           "-c",
-          "hdfs dfs -mkdir -p /apps/hudi/lib; hdfs dfs -copyFromLocal /usr/lib/hudi/hudi-spark-bundle.jar /apps/hudi/lib/hudi-spark-bundle.jar; hdfs dfs -copyFromLocal /usr/lib/spark/external/lib/spark-avro.jar /apps/hudi/lib/spark-avro.jar  ; hdfs dfs -mkdir -p /apps/iceberg/lib; hdfs dfs -copyFromLocal /usr/share/aws/iceberg/lib/iceberg-spark3-runtime.jar /apps/iceberg/lib/iceberg-spark3-runtime.jar"
+          "; ".join([
+            "hdfs dfs -mkdir -p /apps/hudi/lib",
+            "hdfs dfs -copyFromLocal /usr/lib/hudi/hudi-spark-bundle.jar /apps/hudi/lib/hudi-spark-bundle.jar",
+            "hdfs dfs -copyFromLocal /usr/lib/spark/external/lib/spark-avro.jar /apps/hudi/lib/spark-avro.jar",
+            "hdfs dfs -mkdir -p /apps/iceberg/lib",
+            "hdfs dfs -copyFromLocal /usr/share/aws/iceberg/lib/iceberg-spark3-runtime.jar /apps/iceberg/lib/iceberg-spark3-runtime.jar"
+          ])
         ]
       ),
       job_flow_id="copy_jars_step",
