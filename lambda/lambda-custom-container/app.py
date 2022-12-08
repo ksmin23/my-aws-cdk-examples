bfbc336f6109bdb92298666c7152e6f8db8f6de3
ksmin23 <sonagy23@gmail.com>
2022-12-08 16:48:28 +0900

    inital commit - lambda function with custom container

---
+++ lambda/lambda-custom-container/app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from aws_cdk import (
+  Stack,
+  aws_ec2,
+  aws_elasticloadbalancingv2 as elbv2,
+  aws_elasticloadbalancingv2_targets as elbv2_targets,
+  aws_lambda,
+  aws_sam,
+  aws_ecr,
+)
+
+from constructs import Construct
+
+
+class LambdaCustomContainerStack(Stack):
+
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    #XXX: For creating this CDK Stack in the existing VPC,
+    # remove comments from the below codes and
+    # comments out vpc = aws_ec2.Vpc(..) codes,
+    # then pass -c vpc_name=your-existing-vpc to cdk command
+    # for example,
+    # cdk -c vpc_name=your-existing-vpc syth
+    #
+    # if you encounter an error such as:
+    #  jsii.errors.JavaScriptError:
+    #    Error: When providing vpc options you need to provide a subnet for each AZ you are using at new Domain
+    # check https://github.com/aws/aws-cdk/issues/12078
+    # This error occurs when ZoneAwarenessEnabled in aws_opensearch.Domain(..) is set `true`
+    # 
+    vpc_name = self.node.try_get_context('vpc_name')
+    vpc = aws_ec2.Vpc.from_lookup(self, 'ExistingVPC',
+      is_default=True,
+      vpc_name=vpc_name
+    )
+
+    # vpc = aws_ec2.Vpc(self, "AlbLambdaStackVPC",
+    #   max_azs=3,
+    #   gateway_endpoints={
+    #     "S3": aws_ec2.GatewayVpcEndpointOptions(
+    #       service=aws_ec2.GatewayVpcEndpointAwsService.S3
+    #     )
+    #   }
+    # )
+
+    custom_container_ecr_repo = aws_ecr.Repository.from_repository_name(self, "LambdaCustomContainerEcrRepo",
+        repository_name="hello-world")
+
+    lambda_fn = aws_lambda.Function(self, "CustomContainerLambdaFunction",
+      runtime=aws_lambda.Runtime.FROM_IMAGE,
+      function_name="Hello-KoNLpy",
+      handler=aws_lambda.Handler.FROM_IMAGE,
+      description="Lambda function defined in the custom container",
+      code=aws_lambda.Code.from_ecr_image(repository=custom_container_ecr_repo),
+      timeout=cdk.Duration.minutes(5),
+      memory_size=5120
+    )
+
+
+app = cdk.App()
+LambdaCustomContainerStack(app, "LambdaCustomContainerStack",
+  env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+    region=os.getenv('CDK_DEFAULT_REGION')))
+
+app.synth()
