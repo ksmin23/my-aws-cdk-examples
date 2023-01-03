3f6bb92f49fa6f1a7b9a43d482dce55b453d62c3
ksmin23 <sonagy23@gmail.com>
2023-01-03 22:54:11 +0900

    upgrade CDK version to the latest

--- lambda/async-invoke/app.py
+++ lambda/async-invoke/app.py
@@ -27,10 +27,20 @@ class LambdaAsyncInvokeStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
+    #XXX: For creating this CDK Stack in the existing VPC,
+    # remove comments from the below codes and
+    # comments out vpc = aws_ec2.Vpc(..) codes,
+    # then pass -c vpc_name=your-existing-vpc to cdk command
+    # for example,
+    # cdk -c vpc_name=your-existing-vpc syth
+    #
     # vpc_name = self.node.try_get_context("vpc_name")
     # vpc = aws_ec2.Vpc.from_lookup(self, "ExistingVPC",
     #   is_default=True,
     #   vpc_name=vpc_name)
+
+    #XXX: To use more than 2 AZs, be sure to specify the account and region on your stack.
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
     vpc = aws_ec2.Vpc(self, "LambdaAsyncInvokeVPC",
       max_azs=2,
       gateway_endpoints={
