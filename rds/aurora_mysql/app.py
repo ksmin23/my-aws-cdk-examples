98bd2b3c109c55e5c0d7af07d24072bed4c6d0a4
ksmin23 <sonagy23@gmail.com>
2023-01-03 16:05:57 +0900

    upgrade CDK version to the lastest

--- rds/aurora_mysql/app.py
+++ rds/aurora_mysql/app.py
@@ -21,10 +21,28 @@ class AuroraMysqlStack(Stack):
   def __init__(self, scope: Construct, id: str, **kwargs) -> None:
     super().__init__(scope, id, **kwargs)
 
-    vpc_name = self.node.try_get_context("vpc_name")
+    #XXX: For creating this CDK Stack in the existing VPC,
+    # remove comments from the below codes and
+    # comments out vpc = aws_ec2.Vpc(..) codes,
+    # then pass -c vpc_name=your-existing-vpc to cdk command
+    # for example,
+    # cdk -c vpc_name=your-existing-vpc syth
+    #
+    vpc_name = self.node.try_get_context("vpc_name") or "default"
     vpc = aws_ec2.Vpc.from_lookup(self, "ExistingVPC",
       is_default=True,
       vpc_name=vpc_name)
+
+    #XXX: To use more than 2 AZs, be sure to specify the account and region on your stack.
+    #XXX: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
+    # vpc = aws_ec2.Vpc(self, "AuroraMySQLVPC",
+    #   max_azs=2,
+    #   gateway_endpoints={
+    #     "S3": aws_ec2.GatewayVpcEndpointOptions(
+    #       service=aws_ec2.GatewayVpcEndpointAwsService.S3
+    #     )
+    #   }
+    # )
 
     sg_use_mysql = aws_ec2.SecurityGroup(self, 'MySQLClientSG',
       vpc=vpc,
