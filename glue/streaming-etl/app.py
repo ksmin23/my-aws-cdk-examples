e911a07f2e0035c4ce3d4e3a7d9d84eee6512e7b
ksmin23 <sonagy23@gmail.com>
2023-01-10 17:37:54 +0900

    Working on Glue Streaming Job stack

--- glue/streaming-etl/app.py
+++ glue/streaming-etl/app.py
@@ -14,5 +14,7 @@ APP_ENV = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'),
 app = cdk.App()
 
 kds_stack = KdsStack(app, 'KinesisStreamForGlueStreamingJob')
+glue_streaming_job = GlueStreamingJobStack(app, 'GlueStreamingSinkToS3')
+glue_streaming_job.add_dependency(kds_stack)
 
 app.synth()
