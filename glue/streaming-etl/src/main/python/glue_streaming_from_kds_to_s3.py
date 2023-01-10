36c7a4fe7a6b414f165fa3a7d37b237ee107e914
ksmin23 <sonagy23@gmail.com>
2023-01-10 17:37:54 +0900

    Working on Glue Streaming Job stack

--- glue/streaming-etl/src/main/python/glue_streaming_from_kds_to_s3.py
+++ glue/streaming-etl/src/main/python/glue_streaming_from_kds_to_s3.py
@@ -1,7 +1,6 @@
+import os
 import sys
 import datetime
-import boto3
-import base64
 
 from pyspark.sql import DataFrame, Row
 from pyspark.context import SparkContext
@@ -15,8 +14,15 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue import DynamicFrame
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'aws_region', 'output_path'])
+
+args = getResolvedOptions(sys.argv, [
+    'JOB_NAME',
+    'aws_region',
+    'output_path',
+    'glue_database',
+    'glue_table_name',
+    'stream_starting_position'
+])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -24,22 +30,22 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# S3 sink locations
-aws_region = args['aws_region']
-output_path = args['output_path']
+GLUE_DATABASE = args['glue_database']
+GLUE_TABLE_NAME = args['glue_table_name']
+STREAM_STARTING_POSITION = args.get('stream_starting_position', 'LATEST')
 
-s3_target = output_path + "ventilator_metrics"
-checkpoint_location = output_path + "cp/"
-temp_path = output_path + "temp/"
+# S3 sink locations
+AWS_REGION = args['aws_region']
+OUTPUT_PATH = args['output_path']
+
+S3_TARGET = os.path.join(OUTPUT_PATH, "ventilator_metrics")
+CHECKPOINT_LOCATION = os.path.join(OUTPUT_PATH, "cp/")
+TEMP_PATH = os.path.join(OUTPUT_PATH, "temp/")
 
 def processBatch(data_frame, batchId):
-    now = datetime.datetime.now()
-
-    year = now.year
-    month = now.month
-    day = now.day
-    hour = now.hour
-    minute = now.minute
+    _now = datetime.datetime.now()
+    year, month, day, hour = (_now.year, _now.month, _now.day, _now.hour)
+    partition = f"/ingest_year={year:0>4}/ingest_month={month:0>2}/ingest_day={day:0>2}/ingest_hour={hour:0>2}/"
 
     if (data_frame.count() > 0):
         dynamic_frame = DynamicFrame.fromDF(data_frame, glueContext, "from_data_frame")
@@ -53,35 +59,35 @@ def processBatch(data_frame, batchId):
             ("manufacturer", "string", "manufacturer", "string")],
             transformation_ctx = "apply_mapping")
 
-        dynamic_frame.printSchema()
+        # dynamic_frame.printSchema()
 
         # Write to S3 Sink
-        s3path = s3_target + "/ingest_year={year:0>4}/ingest_month={month:0>2}/ingest_day={day:0>2}/ingest_hour={hour:0>2}/".format(year=year, month=month, day=day, hour=hour)
-        s3sink = glueContext.write_dynamic_frame.from_options(frame=apply_mapping, connection_type="s3",
-            connection_options={"path": s3path}, format="parquet", transformation_ctx="s3sink")
+        s3path = os.path.join(S3_TARGET, partition)
+        _s3sink = glueContext.write_dynamic_frame.from_options(
+            frame=apply_mapping,
+            connection_type="s3",
+            connection_options={"path": s3path},
+            format="parquet",
+            transformation_ctx="s3sink")
 
 # Read from Kinesis Data Stream
-sourceData1 = glueContext.create_data_frame.from_catalog(
-    database = "ventilatordb",
-    table_name = "ventilators_table",
+sourceData = glueContext.create_data_frame.from_catalog(
+    database = GLUE_DATABASE, # "ventilatordb",
+    table_name = GLUE_TABLE_NAME, # "ventilators_table",
     transformation_ctx = "datasource1",
-    additional_options = {"startingPosition": "TRIM_HORIZON", "inferSchema": "true"})
+    additional_options = {
+        "startingPosition": STREAM_STARTING_POSITION,
+        "inferSchema": "true"
+    }
+)
 
-sourceData1.printSchema()
+# sourceData1.printSchema()
 
-# sourceData2 = glueContext.create_data_frame.from_catalog(
-#     database = "ventilatordb",
-#     table_name = "ventilators_table2",
-#     transformation_ctx = "datasource2",
-#     additional_options = {"startingPosition": "TRIM_HORIZON", "inferSchema": "true"})
-
-# sourceData2.printSchema()
-
-glueContext.forEachBatch(frame=sourceData1,
+glueContext.forEachBatch(frame=sourceData,
     batch_function=processBatch,
     options = {
         "windowSize": "100 seconds",
-        "checkpointLocation": checkpoint_location
+        "checkpointLocation": CHECKPOINT_LOCATION
     })
 
 job.commit()
