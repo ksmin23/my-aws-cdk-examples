cb08769a8cef64c71d2e17b53423ef2ca1eec68d
ksmin23 <sonagy23@gmail.com>
2023-01-14 22:06:01 +0900

    add Streaming Data into Iceberg table using Glue

---
+++ glue/streaming-etl/sink-to-iceberg/cdk_stacks/kds.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import random
+import string
+
+import aws_cdk as cdk
+
+from aws_cdk import (
+  Duration,
+  Stack,
+  aws_kinesis,
+)
+from constructs import Construct
+
+random.seed(23)
+
+class KdsStack(Stack):
+
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    KINESIS_DEFAULT_STREAM_NAME = 'PUT-{}'.format(''.join(random.sample((string.ascii_letters), k=5)))
+    KINESIS_STREAM_NAME = self.node.try_get_context('kinesis_stream_name') or KINESIS_DEFAULT_STREAM_NAME
+
+    source_kinesis_stream = aws_kinesis.Stream(self, "SourceKinesisStreams",
+      retention_period=Duration.hours(24),
+      stream_mode=aws_kinesis.StreamMode.ON_DEMAND,
+      stream_name=KINESIS_STREAM_NAME)
+
+    self.kinesis_stream = source_kinesis_stream
+
+    cdk.CfnOutput(self, f'{self.stack_name}_KinesisDataStreamName', value=self.kinesis_stream.stream_name)
+
---
+++ glue/streaming-etl/sink-to-iceberg/src/utils/gen_fake_kinesis_stream_data.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+
+import sys
+import argparse
+from datetime import datetime
+import json
+import random
+import time
+
+import boto3
+from mimesis.locales import Locale
+from mimesis.schema import Field, Schema
+
+
+def main():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--region-name', action='store', default='us-east-1',
+    help='aws region name (default: us-east-1)')
+  parser.add_argument('--stream-name', help='The name of the stream to put the data record into')
+  parser.add_argument('--max-count', default=10, type=int, help='The max number of records to put (default: 10)')
+  parser.add_argument('--dry-run', action='store_true')
+
+  options = parser.parse_args()
+
+  _CURRENT_YEAR = datetime.now().year
+
+  #XXX: For more information about synthetic data schema, see
+  # https://github.com/aws-samples/aws-glue-streaming-etl-blog/blob/master/config/generate_data.py
+  _ = Field(locale=Locale.EN)
+
+  _schema = Schema(schema=lambda: {
+    "name": _("first_name"),
+    "age": _("age"),
+  })
+
+  if not options.dry_run:
+    kinesis_streams_client = boto3.client('kinesis', region_name=options.region_name)
+
+  cnt = 0
+  for record in _schema.iterator(options.max_count):
+    cnt += 1
+
+    if options.dry_run:
+      print(f"{json.dumps(record)}\n")
+    else:
+      res = kinesis_streams_client.put_record(
+        StreamName=options.stream_name,
+        Data=f"{json.dumps(record)}\n", # convert JSON to JSON Line
+        PartitionKey=f"{record['name']}"
+      )
+
+      if cnt % 100 == 0:
+        print(f'[INFO] {cnt} records are processed', file=sys.stderr)
+
+      if res['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print(res, file=sys.stderr)
+    time.sleep(random.choices([0.01, 0.03, 0.05, 0.07, 0.1])[-1])
+  print(f'[INFO] Total {cnt} records are processed', file=sys.stderr)
+
+
+if __name__ == '__main__':
+  main()
