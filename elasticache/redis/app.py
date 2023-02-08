3276ba61744602cc8bf5682fd2f9c807e0de95e3
ksmin23 <sonagy23@gmail.com>
2023-02-08 13:48:38 +0900

    update VPC Stack

--- elasticache/redis/app.py
+++ elasticache/redis/app.py
@@ -17,6 +17,19 @@ class RedisStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
+    #XXX: For createing Amazon MWAA in the existing VPC,
+    # remove comments from the below codes and
+    # comments out vpc = aws_ec2.Vpc(..) codes,
+    # then pass -c vpc_name=your-existing-vpc to cdk command
+    # for example,
+    # cdk -c vpc_name=your-existing-vpc syth
+    #
+    # vpc_name = self.node.try_get_context('vpc_name')
+    # vpc = aws_ec2.Vpc.from_lookup(self, 'ExistingVPC',
+    #   is_default=True,
+    #   vpc_name=vpc_name
+    # )
+
     vpc = aws_ec2.Vpc(self, 'RedisVPC',
       max_azs=2
     )
