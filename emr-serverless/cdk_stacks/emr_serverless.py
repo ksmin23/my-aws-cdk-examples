1cb5f096d834f5edf378b7ca0b4952c42921eca6
ksmin23 <sonagy23@gmail.com>
2024-08-04 19:03:00 +0900

    update ERM Serverless CDK Project

--- emr-serverless/cdk_stacks/emr_serverless.py
+++ emr-serverless/cdk_stacks/emr_serverless.py
@@ -7,6 +7,7 @@ import aws_cdk as cdk
 from aws_cdk import (
   Stack,
   aws_emrserverless,
+  aws_iam
 )
 
 from constructs import Construct
@@ -23,8 +24,70 @@ class EmrServerlessStack(Stack):
       default='my-spark-app'
     )
 
+    emr_serverless_s3_and_glue_access_policy_doc = aws_iam.PolicyDocument()
+
+    emr_serverless_s3_and_glue_access_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "sid": "ReadAccessForEMRSamples",
+      "effect": aws_iam.Effect.ALLOW,
+      "actions": [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "resources": [
+        "arn:aws:s3:::*.elasticmapreduce",
+        "arn:aws:s3:::*.elasticmapreduce/*"
+      ]
+    }))
+
+    emr_serverless_s3_and_glue_access_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "sid": "FullAccessToOutputBucket",
+      "effect": aws_iam.Effect.ALLOW,
+      "actions": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:DeleteObject"
+      ],
+      "resources": [
+        "arn:aws:s3:::*deltalake*",
+        "arn:aws:s3:::*deltalake*/*"
+      ]
+    }))
+
+    emr_serverless_s3_and_glue_access_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "sid": "GlueCreateAndReadDataCatalog",
+      "effect": aws_iam.Effect.ALLOW,
+      "actions": [
+        "glue:GetDatabase",
+        "glue:CreateDatabase",
+        "glue:GetDataBases",
+        "glue:CreateTable",
+        "glue:GetTable",
+        "glue:UpdateTable",
+        "glue:DeleteTable",
+        "glue:GetTables",
+        "glue:GetPartition",
+        "glue:GetPartitions",
+        "glue:CreatePartition",
+        "glue:BatchCreatePartition",
+        "glue:GetUserDefinedFunctions"
+      ],
+      "resources": [
+        "*"
+      ]
+    }))
+
+    emr_serverless_s3_runtime_role = aws_iam.Role(self, 'EmrServerlessS3RuntimeRole',
+      role_name='EMRServerlessS3RuntimeRole',
+      assumed_by=aws_iam.ServicePrincipal('elasticmapreduce.amazonaws.com'),
+      inline_policies={
+        'EMRServerlessS3AndGlueAccessPolicy': emr_serverless_s3_and_glue_access_policy_doc
+      }
+    )
+
+    emr_version = self.node.try_get_context('emr_version') or "emr-7.2.0"
     emr_serverless_app = aws_emrserverless.CfnApplication(self, "MyEmrServerlessApp",
-      release_label="emr-6.6.0",
+      release_label=emr_version,
       type="Spark",
 
       # the properties below are optional
@@ -33,14 +96,17 @@ class EmrServerlessStack(Stack):
       ),
       auto_stop_configuration=aws_emrserverless.CfnApplication.AutoStopConfigurationProperty(
         enabled=True,
-        idle_timeout_minutes=15
+        idle_timeout_minutes=360
       ),
       initial_capacity=[aws_emrserverless.CfnApplication.InitialCapacityConfigKeyValuePairProperty(
         key="Driver",
         value=aws_emrserverless.CfnApplication.InitialCapacityConfigProperty(
           worker_configuration=aws_emrserverless.CfnApplication.WorkerConfigurationProperty(
-            cpu="2vCPU",
-            memory="4GB"
+            cpu="4vCPU",
+            memory="16GB",
+
+            # the properties below are optional
+            disk="32GB"
           ),
           worker_count=2
         )
@@ -50,7 +116,10 @@ class EmrServerlessStack(Stack):
         value=aws_emrserverless.CfnApplication.InitialCapacityConfigProperty(
           worker_configuration=aws_emrserverless.CfnApplication.WorkerConfigurationProperty(
             cpu="4vCPU",
-            memory="8GB"
+            memory="16GB",
+
+            # the properties below are optional
+            disk="32GB"
           ),
           worker_count=10
         )
@@ -62,6 +131,7 @@ class EmrServerlessStack(Stack):
       ),
       name=EMR_SERVERLESS_APP_NAME.value_as_string
     )
+
 
     cdk.CfnOutput(self, 'ApplicationName', value=emr_serverless_app.name,
       export_name=f'{self.stack_name}-ApplicationName')
